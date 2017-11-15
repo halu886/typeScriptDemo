d564aae259ac6196930015167ff1ba93b25a597b
halu <halu886@qq.com>
2017-11-15 09:16:14 +0800

    抽象类

--- class.ts
+++ class.ts
@@ -148,16 +148,106 @@
 // dad.name = "Man with the 3-piece suit";
 
 
-// 参数属性
-class Animal {
-    constructor(private name: string) {
+// // 参数属性
+// class Animal {
+//     constructor(private name: string) {
+
+//     }
+//     move(distanceInMeters: number) {
+//         console.log(`${this.name} moved ${distanceInMeters}m.`)
+//     }
+// }
+
+// let animal = new Animal('cat');
+// // console.log()
+// animal.move(223132)
+
+
+// // 存取器
+// class Employee {
+//     fullName: string;
+// }
+// let employee = new Employee();
+// employee.fullName = "Bob Smith";
+// if (employee.fullName) {
+//     console.log(employee.fullName);
+// }
+
+// let passcode = `secret passcode`;
+
+// class Employee {
+//     private _fullName: string;
+//     get fullName(): string {
+//         return this._fullName;
+//     }
+//     set fullName(newName: string) {
+//         if (passcode && passcode == "secret passcode") {
+//             this._fullName = newName;
+//         }
+//         else {
+//             console.log("Error:Unauthorized update of employee!")
+//         }
+//     }
+// }
+
+
+// let employee = new Employee();
+// employee.fullName = "Bob Smith";
+// if (employee.fullName) {
+//     console.log(employee.fullName)
+// }
+
+// // 静态属性
+// class Grid {
+//     static origin = { x: 0, y: 0 };
+//     calculateDistanceFromOrigin(point: { x: number, y: number }) {
+//         let xDist = (point.x - Grid.origin.x);
+//         let yDist = (point.y - Grid.origin.y);
+//         return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale
+//     }
+//     constructor(public scale: number) { }
+// }
+
+// let grid1 = new Grid(1.0);
+// let grid2 = new Grid(5.0);
+// console.log(grid1.calculateDistanceFromOrigin({ x: 10, y: 10 }), grid2.calculateDistanceFromOrigin({ x: 10, y: 10 }))
+
+
+// 抽象类
+// abstract class Animal {
+//     abstract makeSound(): void;
+//     move(): void {
+//         console.log("roming the earch……")
+//     }
+// }
+
+abstract class Department {
+    constructor(public name: string) {
 
     }
-    move(distanceInMeters: number) {
-        console.log(`${this.name} moved ${distanceInMeters}m.`)
+
+    printName(): void {
+        console.log("Department name " + this.name);
+    }
+
+    abstract printMeeting(): void;
+}
+
+class AccountDepartment extends Department {
+    constructor() {
+        super('Accounting and Auditing')
+    }
+    generateReports(): void {
+        console.log('Generating accounting reports……')
+    }
+    printMeeting(): void {
+        console.log('The Accounting Department meets each Monday at 10am')
     }
 }
 
-let animal = new Animal('cat');
-// console.log()
-animal.move(223132)
+let department: Department;
+// department = new Department();
+department = new AccountDepartment();
+department.printName();
+department.printMeeting();
+// department.generateReports()
