b779df4862a007c5a62e43221eee3446962a1985
halu <halu886@qq.com>
2017-11-17 08:58:52 +0800

    枚举

---
+++ enumn.ts
@@ -0,0 +1,40 @@
+/**
+ * @description 枚举
+ * @data 2017/11/17
+ * @author halu
+ */
+
+// enum Derection {
+//     up = 1,
+//     Down,
+//     left,
+//     Right
+// }
+
+// enum FileAccess {
+//     None,
+//     Read = 1 << 1,
+//     Write = 1 << 2,
+//     ReedWrite = Read | Write,
+//     G = "1234".length
+// }
+
+// // enum Enum {
+// //     A
+// // }
+
+// let a = Enum.A;
+// let nameA = Enum[a];
+
+// const enum Enum {
+//     A = 1,
+//     B = A * 2,
+// }
+
+
+// 外部枚举
+declare enum Enum {
+    A = 1,
+    B,
+    C = 2,
+}
